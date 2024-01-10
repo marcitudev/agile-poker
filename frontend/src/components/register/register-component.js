e975d37def178e699fc1f85bec58e70b54f094cd
Márcio Costa <mncosta99@gmail.com>
2024-01-09 23:18:54 -0300

    feat: initial register template

--- frontend/src/components/register/register-component.js
+++ frontend/src/components/register/register-component.js
@@ -1,6 +1,8 @@
 import htmlContent from './register-component.html';
 import './register-component.css';
 import './../../styles/main.css';
+import './../../styles/fonts.css';
+import './../../styles/form.css';
 
 class Register extends HTMLElement{
     constructor(){
