6d85eca97bff32414791912bf8c839860c0a9a6d
Márcio Costa <mncosta99@gmail.com>
2024-01-11 15:48:11 -0300

    feat: base service created

---
+++ frontend/src/services/base-service.js
@@ -0,0 +1,45 @@
+export default class BaseService{
+    get(url, params = {}){
+        return this.request('GET', url, params);
+    }
+
+    post(url, params = {}){
+        return this.request('POST', url, params);
+    }
+    
+    put(url, params = {}){
+        return this.request('PUT', url, params);
+    }
+
+    delete(url, params = {}){
+        return this.request('DELETE', url, params);
+    }
+
+    request(method, url, params = {}){
+        return new Promise((resolve, reject) => {
+            let request;
+            
+            switch(method.toUpperCase()){
+                case 'GET':
+                    request = {
+                        method
+                    };
+                    break;
+                default:
+                    request = {
+                        method,
+                        body: JSON.stringify(params),
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    };
+            }
+
+            fetch(url, request).then(response => {
+                response.json().then(json => {
+                    resolve(json);
+                }).catch(e => reject(e))
+            }).catch(e => reject(e))
+        });
+    }
+}
