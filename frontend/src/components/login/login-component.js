ff7356bc814acae690e07f8e6110bc8157e93493
Márcio Costa <mncosta99@gmail.com>
2024-01-19 17:04:57 -0300

    feat: authentication request created in login component

--- frontend/src/components/login/login-component.js
+++ frontend/src/components/login/login-component.js
@@ -1,3 +1,4 @@
+import AuthenticationService from '../../services/authentication-service';
 import htmlContent from './login-component.html';
 import './login-component.scss';
 
@@ -8,6 +9,25 @@ class Login extends HTMLElement{
     
     connectedCallback(){
         this.innerHTML = htmlContent;
+        this.service = new AuthenticationService();
+        this.submit();
+    }
+
+    submit(){
+        const formEl = document.querySelector('#login-form');
+        const inputsEl = formEl.querySelectorAll('input');
+
+        formEl.addEventListener('submit', (event) => {
+            event.preventDefault();
+
+            const authUser = {};
+            [...inputsEl].forEach(element => {
+                authUser[element.name] = element.value;
+            });
+
+            const {username, password} = authUser;
+            this.service.authenticate(username, password);
+        });
     }
 }
 
