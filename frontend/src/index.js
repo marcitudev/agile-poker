b46c6321788de079b742e7bb873f626b63bf10cd
Márcio Costa <mncosta99@gmail.com>
2024-02-02 15:38:57 -0300

    feat: home component added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import routing from './router';
 import './styles/main.scss';
 import './components/login/login-component';
 import './components/register/register-component';
+import './components/home/home-component';
 import TranslateService from './services/translate-service';
 
 new TranslateService().init();
