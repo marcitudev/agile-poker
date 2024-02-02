2ccafbe54ba1372799589350493da31f32c9a553
Márcio Costa <mncosta99@gmail.com>
2024-02-02 15:37:56 -0300

    feat: auth guard created

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,14 +1,36 @@
 import { Router } from '@vaadin/router';
+import AuthenticationService from './services/authentication-service';
+import TranslateService from './services/translate-service';
+import Toastr from './components/toastr/toastr-component';
 
 const routes = [
     { path: '/', redirect: '/login' },
-    { path: '/login', component: 'app-login' },
-    { path: '/register', component: 'app-register' }
+    { path: '/login', component: 'app-login', action: (context) => { authGuard(context); } },
+    { path: '/register', component: 'app-register', action: (context) => { authGuard(context); }},
+    { path: '/home', component: 'app-home', action: (context) => { authGuard(context); }}
 ]
 
 const routing = (outlet) => {
     const router = new Router(outlet);
     router.setRoutes(routes);
+}
+
+const authenticationService = new AuthenticationService();
+const translateService = new TranslateService();
+const toastrService = new Toastr();
+const routesWithoutAuthentication = ['/login', '/register'];
+const authGuard = (context) => {
+    authenticationService.verifyTokenExpiration().then(() => {
+        if(routesWithoutAuthentication.includes(context.pathname)){
+            Router.go('/home');
+        }
+    }).catch(() => {
+        if(!routesWithoutAuthentication.includes(context.pathname)){
+            const message = translateService.getTranslation('errors.session-expired');
+            toastrService.error(message);
+            Router.go('/login');
+        }
+    });
 }
 
 export default routing;
