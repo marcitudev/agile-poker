d22df57660f12823ff715ed298086e68b0c66044
Márcio Costa <mncosta99@gmail.com>
2024-02-09 15:40:49 -0300

    feat: navbar added in index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import './styles/main.scss';
 import './components/login/login-component';
 import './components/register/register-component';
 import './components/home/home-component';
+import './components/navbar/navbar-component';
 import TranslateService from './services/translate-service';
 
 new TranslateService().init();
