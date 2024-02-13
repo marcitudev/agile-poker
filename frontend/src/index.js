b079e6ca39c4d2f50f0ec8adf2c671626a1b3a0e
Márcio Costa <mncosta99@gmail.com>
2024-02-13 17:46:44 -0300

    feat: remove navbar when remove navbar when on the login or registration screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,29 @@ import './components/login/login-component';
 import './components/register/register-component';
 import './components/home/home-component';
 import './components/navbar/navbar-component';
+import './components/translation/translation-component';
 import TranslateService from './services/translate-service';
 
+const outlet = document.querySelector('#outlet');
+
+const observer = new MutationObserver(() => {
+    let navbarEl = document.body.querySelector('app-navbar');
+
+    const element = outlet.firstElementChild;
+    if(element?.tagName.toLowerCase() === 'app-login' || element?.tagName.toLowerCase() === 'app-register') {
+        if (navbarEl && document.body.contains(navbarEl)) {
+            document.body.removeChild(navbarEl);
+        }
+    } else {
+        if(!navbarEl){
+            navbarEl = document.createElement('app-navbar');
+            document.body.insertBefore(navbarEl, outlet);
+        }
+    }
+});
+
+const config = { childList: true, subtree: false, attributes: false };
+observer.observe(outlet, config);
+
 new TranslateService().init();
-routing(document.querySelector('#outlet'));
+routing(outlet);
