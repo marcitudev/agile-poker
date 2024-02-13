9a6bde402650e3dee3ba8a0351bf06c6f1612acd
Márcio Costa <mncosta99@gmail.com>
2024-02-13 18:09:53 -0300

    feat: index cleaning

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,5 @@
 import routing from './router';
+import { handlerNavbar } from './app-setup';
 import './styles/main.scss';
 import './components/login/login-component';
 import './components/register/register-component';
@@ -9,24 +10,6 @@ import TranslateService from './services/translate-service';
 
 const outlet = document.querySelector('#outlet');
 
-const observer = new MutationObserver(() => {
-    let navbarEl = document.body.querySelector('app-navbar');
-
-    const element = outlet.firstElementChild;
-    if(element?.tagName.toLowerCase() === 'app-login' || element?.tagName.toLowerCase() === 'app-register') {
-        if (navbarEl && document.body.contains(navbarEl)) {
-            document.body.removeChild(navbarEl);
-        }
-    } else {
-        if(!navbarEl){
-            navbarEl = document.createElement('app-navbar');
-            document.body.insertBefore(navbarEl, outlet);
-        }
-    }
-});
-
-const config = { childList: true, subtree: false, attributes: false };
-observer.observe(outlet, config);
-
 new TranslateService().init();
 routing(outlet);
+handlerNavbar(outlet);
