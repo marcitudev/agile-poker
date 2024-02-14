5ae1ccd987d08d1f82531204c7aacec1d22f0342
= <Marcio.Nascimento@fit-tecnologia.org.br>
2024-02-14 16:28:26 -0300

    feat: open and close profile dropdown

--- frontend/src/components/navbar/navbar-component.js
+++ frontend/src/components/navbar/navbar-component.js
@@ -1,6 +1,8 @@
 import AuthenticationService from '../../services/authentication-service';
+import TranslateService from '../../services/translate-service';
 import htmlContent from './navbar-component.html';
 import './navbar-component.scss';
+import { Router } from '@vaadin/router';
 
 export class Navbar extends HTMLElement{
     constructor(){
@@ -10,7 +12,11 @@ export class Navbar extends HTMLElement{
     connectedCallback(){
         this.innerHTML = htmlContent;
         this.authenticationService = new AuthenticationService();
+        this.translateService = new TranslateService();
         this.showUsername();
+        this.openAndCloseProfileOptions();
+        this.addProfileOptionsEvents();
+        this.translateService.translate();
     }
 
     showUsername(){
@@ -19,6 +25,32 @@ export class Navbar extends HTMLElement{
             const usernameEl = document.querySelector('.username');
             usernameEl.textContent = authUser.username;
         }
+    }
+
+    openAndCloseProfileOptions(){
+        const profileEl = document.querySelector('.profile-img');
+        const profileOptionsEl = profileEl.querySelector('.profile-options');
+        profileOptionsEl.style.display = 'none';
+
+        profileEl.addEventListener('click', () => {
+            const display = profileOptionsEl.style.display;
+            profileOptionsEl.style.display = display === 'none' ? 'block' : 'none';
+        });
+
+        document.addEventListener('click', (event) => {
+            if(!profileEl.contains(event.target)){
+                profileOptionsEl.style.display = 'none';
+            }
+        });
+    }
+
+    addProfileOptionsEvents(){
+        const logoutEl = document.querySelector('.logout');
+
+        logoutEl.addEventListener('click', () => {
+            this.authenticationService.clearToken();
+            Router.go('/login');
+        });
     }
 }
 
