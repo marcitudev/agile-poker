6110fbd2e8b89823bfcbbb9e2d13c4a79a2b0b7a
Márcio Costa <mncosta99@gmail.com>
2024-03-05 17:50:42 -0300

    feat: implement name, password, card value types and action buttons in modal

--- frontend/src/modals/create-room/create-room-modal.js
+++ frontend/src/modals/create-room/create-room-modal.js
@@ -1,5 +1,7 @@
 import htmlContent from './create-room-modal.html';
 import './create-room-modal.scss';
+import { SEQUENTIAL } from '../../utils/card-value-type-constants';
+import { FIBONACCI } from '../../utils/card-value-type-constants';
 
 class CreateRoom extends HTMLElement{
 
@@ -9,8 +11,36 @@ class CreateRoom extends HTMLElement{
 
     connectedCallback(){
         this.innerHTML = htmlContent;
+        this.togglePassword();
+        this.cardValueTypeSelection();
+    }
+
+    togglePassword(){
+        const passwordToggleEl = document.querySelector('#password-toggle');
+        passwordToggleEl.addEventListener('change', (event) => {
+            const passwordInputEl = document.querySelector('#password-input');
+            passwordInputEl.disabled = event.target.checked ? false : true;
+        });
+    }
+
+    cardValueTypeSelection(){
+        const cardValueTypesEl = document.querySelector('.card-values');
+        const cardValueTypesOptionsEl = cardValueTypesEl.querySelectorAll('.card-value-type');
+        [...cardValueTypesOptionsEl].forEach(cardValueType => {
+            cardValueType.dataset.type = cardValueType.id === 'sequential' ? SEQUENTIAL : FIBONACCI;
+            cardValueType.addEventListener('click', () => {
+                [...cardValueTypesOptionsEl].map(cardValueTypeEl => {
+                    cardValueTypeEl.removeAttribute('selected');
+                    cardValueTypeEl.classList.remove('selected');
+                });
+                cardValueType.setAttribute('selected', '');
+                cardValueType.classList.add('selected');
+                const cardValueTypeExample = document.querySelector('#card-value-type-example');
+                cardValueTypeExample.innerHTML = cardValueType.dataset.type.replaceAll(',', ' ');
+            });
+        });
     }
 
 }
 
-customElements.define('app-create-room', CreateRoom);
+customElements.define('modal-create-room', CreateRoom);
